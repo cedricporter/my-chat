dddf24dd6b500e01da7934c1cf3ca9b6072e56a8
Cedric Porter <cedricporter@gmail.com>
2013-02-20 22:12:19 +0800

    Post Message to observer

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ import tornado.web
 import tornado.ioloop
 import os
 import logging
-import random
+import uuid
 
 
 logger = logging.getLogger()
- # logger.setLevel(logging.DEBUG)
+# logger.setLevel(logging.INFO)
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -21,6 +21,8 @@ class MainHandler(tornado.web.RequestHandler):
 class TimeMixin(object):
     observers = []
     count = 0
+    cache = []
+
     def register(self, observer):
         cls = TimeMixin
         cls.observers.append(observer)
@@ -29,28 +31,27 @@ class TimeMixin(object):
         cls = TimeMixin
         cls.observers.remove(observer)
 
-    def alert(self):
+    def alert(self, message):
         cls = TimeMixin
+        cls.cache.append(message)
         cls.count += 1
         obs = cls.observers
         cls.observers = []
         for ob in obs:
-            ob("Count: %d, Len: %d" % (cls.count, len(obs)))
+            ob(message)
         print "----- Observer Len: %d -----" % len(obs)
 
 
 class TimeHandler(tornado.web.RequestHandler, TimeMixin):
     @tornado.web.asynchronous
-    def get(self):
-         # logger.info("----- register: %d -----" % id(self))
+    def post(self):
         print "----- register: %d -----" % id(self)
         self.register(self.onMsg)
 
     def onMsg(self, msg):
         if self.request.connection.stream.closed():
             return
-        self.finish(msg)
-         # logger.info("----- end %d -----" % id(self))
+        self.finish(dict(messages=msg))
         print "----- end %d -----" % id(self)
 
     def on_connection_close(self):
@@ -60,6 +61,22 @@ class TimeHandler(tornado.web.RequestHandler, TimeMixin):
 class UpdateTimeHandler(tornado.web.RequestHandler, TimeMixin):
     def get(self):
         self.alert()
+
+    def post(self):
+        from pprint import pprint
+
+        message = {
+            "id": str(uuid.uuid4()),
+            "from": "user",
+            "body": self.get_argument("body"),
+        }
+        message["html"] = "<div>%s</div>" % message["body"]
+
+        pprint(message)
+
+        self.alert([message])
+
+        self.write(message)
 
 
 application = tornado.web.Application([
--- static/chat.js
+++ static/chat.js
@@ -1,11 +1,74 @@
 $(document).ready(function() {
+
+    $("#msgform").on("submit", function() {
+	newMessage($(this));
+	return false;
+    });
+
     updater.poll();
 });
+
+
+jQuery.postJSON = function(url, args, callback) {
+    // args._xsrf = getCookie("_xsrf");
+    $.ajax({url: url,
+            data: $.param(args),
+            dataType: "text",
+            type: "POST",
+            success: function(response) {
+                if (callback)
+                    callback(eval("(" + response + ")"));
+            },
+            error: function(response) {
+                console.log("ERROR:", response);
+            }});
+};
+
+jQuery.fn.formToDict = function() {
+    var fields = this.serializeArray();
+    var json = {};
+    for (var i = 0; i < fields.length; i++) {
+        json[fields[i].name] = fields[i].value;
+    }
+    if (json.next) delete json.next;
+    return json;
+};
+
+jQuery.fn.disable = function() {
+    this.enable(false);
+    return this;
+};
+
+jQuery.fn.enable = function(opt_enable) {
+    if (arguments.length && !opt_enable) {
+        this.attr("disabled", "disabled");
+    } else {
+        this.removeAttr("disabled");
+    }
+    return this;
+};
+
+
+function newMessage(form) {
+    var msg = form.formToDict();
+    var disabled = form.find("input[type=submit]");
+    disabled.disable();
+
+    $.postJSON("/update", msg, function(response) {
+        if (msg.id) {
+        }
+        else {
+
+	    disabled.enable();
+        }
+    });
+}
+
 
 var updater = {
     poll: function () {
 	$.ajax({url: "/time",
-		type: "GET",
+		type: "POST",
 		dataType: "text",
 		success: updater.onSuccess,
 		error: updater.onError,
@@ -13,13 +76,15 @@ var updater = {
     },
 
     onSuccess: function(response) {
-	$("#msg")[0].innerHTML = response;
+        var r = eval("(" + response + ")");
+        var messages = r.messages;
+	$("#msg").append(messages[messages.length - 1].html);
 
 	window.setTimeout(updater.poll, 0);
     },
 
     onError: function(response) {
-	$("#error")[0].innerHTML += "[ " + response + " ]" + "<br/>";
+	$("#error")[0].innerHTML = "[ " + response + " ]" + "<br/>";
 	window.setTimeout(updater.poll, 500);
     },
 
